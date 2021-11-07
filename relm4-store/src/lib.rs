578d50aa8aba76f826015c5f5cc65b7cc28f9231
Marek <marek@constellationsoft.com>
2021-11-07 04:57:13 +0100

    Documentation for store and store view

--- relm4-store/src/lib.rs
+++ relm4-store/src/lib.rs
@@ -84,8 +84,68 @@ pub trait DataStoreListenable: IdentifiableStore + DataStoreBase {
     fn unlisten(&self, handler_ref: StoreId<Self>);
 }
 
+/// DataStore is a trait describing collections specialized in housekeeping business model data
+/// 
+/// ## DataStore implementations are collections
+/// 
+/// That means they own (in terms of borrow checker) some instances of your business model data.
+/// Also it must give you ability to query your data so you can see what's inside.
+/// 
+/// ## DataStore implementations are housekeeping your data
+/// 
+/// DataStore is a place which knows how to mange CRUD operations on your business data.
+/// Different implementations of this trait might use different strategies to keep your data. 
+/// Some may keep it in the files on the local file system. Some others might store it in database.
+/// Wherever your data are being kept data store needs to track when they have been changed and 
+/// sometimes how so they can store them. 
+/// 
+/// ## When can I use it?
+/// 
+/// You can use data store if it can own the data safely. This boils down to three requirements
+/// 
+/// - no internal mutation<br>
+///   Data store needs to be certain about the data which it holds
+/// - they are serializable<br>
+///   Data store needs to be able to create a copies of the data
+/// - identifiable
+///   In presence of copies data store needs to be able to tell which data is just a different 
+///   representation of another instance of data
+/// 
+/// ### No internal mutation
+/// 
+/// If you create a model which has internal mutability then it would need know to which data store
+/// it belongs to so it can notify it about the changes. But data store is collection so it needs to
+/// know the data which are kept inside. So we have a perfect circle of dependencies which would make
+/// it insane in terms of implementation. What's more it would be store implementation dependant. Which
+/// would make it hard for you to change the way in which you store the data from application point of view.
+/// Changing storage is hard enough without that so no internal mutation.
+/// 
+/// ### They are serializable
+/// 
+/// DataStore implementations needs to be able to save a data for example in a file and read from it.
+/// Also since there is no internal mutation allowed that means data store needs to be able to give you
+/// copies of data inside of it so you can mutate them. If you disallow internal mutation rust's [Clone]
+/// is good enough for generic case. Specific implementations might have extra serialization requirements. 
+/// 
+/// ### They are identifiable
+/// 
+/// DataStore gives you the copy of a data so you can mutate it. Now you would like to update the data
+/// in the store. But which data you are talking about? Equality operation is not going to work since
+/// you just mutated them. That's way your model needs to provide an identifier which is good enough
+/// so the data store can depend on it to identify the instances of the data.
 pub trait DataStore: IdentifiableStore + DataStoreBase + DataStoreListenable{}
 
+/// StoreView allows you to access part of the data in the data store
+/// 
+/// StoreView is a special kind of data store which tracks subset of the data in the other data store.
+/// This is useful in various scenarios
+/// 
+/// - showing data to the user<br>
+///   Showing all ten billion of records at the same time is time consuming and will overwhelm user
+/// - editing the data<br>
+///   Your business model has two data sets `A` and `B` and there is `1-*` relationship between the data.
+///   There are valid scenarios when you would like to edit item in `A` and give the ability to modify
+///   related items in `B` at the same time. 
 pub trait StoreView: DataStore
 {
     type Builder: FactoryBuilder;
