5203df5d87810236de29111c8d466e837ba31655
Marek <marek@constellationsoft.com>
2022-01-12 17:44:26 +0100

    Remove right touches only affected widgets
    Remove right now performs the check which widgets were affected and
    properly marks them as removed or to be updated
    
    Widgets are marked as requireing update if they'r order in the store has
    changed

---
+++ relm4-store-view-implementation/src/implementation/data_container/tests/remove_right.rs
@@ -0,0 +1,284 @@
+use backend_dummy::test_cases::TestRecord;
+use record::Record;
+
+use crate::WindowChangeset;
+use crate::implementation::data_container::DataContainer;
+
+use super::test_data::TestData;
+
+
+#[test]
+fn remove_right_from_empty_container() {
+    let mut dc: DataContainer<TestRecord> = DataContainer::new(10);
+    let mut changeset: WindowChangeset<TestRecord> = WindowChangeset::default();
+
+    let records = vec![];
+
+    dc.remove_right(&mut changeset, 0, 3, records);
+    dc.invariants();
+
+    assert_eq!(dc.len(), 0);
+}
+
+#[test]
+fn remove_right_from_empty_container_outside_of_the_range() {
+    let mut dc: DataContainer<TestRecord> = DataContainer::new(10);
+    let mut changeset: WindowChangeset<TestRecord> = WindowChangeset::default();
+
+    let records = vec![];
+
+    dc.remove_right(&mut changeset, 5, 3, records);
+    dc.invariants();
+
+    assert_eq!(dc.len(), 0);
+}
+
+#[test]
+fn remove_last_element() {
+    let TestData{ records, mut container } = TestData::new(1, 10);
+    let mut changeset: WindowChangeset<TestRecord> = WindowChangeset::default();
+
+    assert_eq!(records.len(), 1);
+    assert_eq!(container.len(), 1);
+
+    container.remove_right(&mut changeset, 0, 1, vec![]);
+
+    assert_eq!(container.len(), 0);
+    assert!(changeset.widgets_to_remove.contains(&records[0].get_id()));
+}
+
+
+mod records_3{
+    use super::Record;
+    use super::TestData;
+    use super::TestRecord;
+    use super::WindowChangeset;
+
+    const RECORDS_CNT: usize = 3;
+    const MAX_SIZE: usize = 10;
+
+    #[test]
+    fn remove_first_element_insert_0() {
+        let TestData{ records, mut container } = TestData::new(RECORDS_CNT, MAX_SIZE);
+        let mut changeset: WindowChangeset<TestRecord> = WindowChangeset::default();
+
+        assert_eq!(records.len(), 3);
+        assert_eq!(container.len(), 3);
+
+        container.remove_right(&mut changeset, 0, 1, vec![]);
+
+        assert_eq!(container.len(), 2);
+        assert_eq!(container.data[&records[1].get_id()], records[1]);
+        assert_eq!(container.data[&records[2].get_id()], records[2]);
+        assert_eq!(container.order[0], records[1].get_id());
+        assert_eq!(container.order[1], records[2].get_id());
+        assert!(changeset.widgets_to_remove.contains(&records[0].get_id()));
+    }
+
+    #[test]
+    fn remove_second_element_insert_0() {
+        let TestData{ records, mut container } = TestData::new(RECORDS_CNT, MAX_SIZE);
+        let mut changeset: WindowChangeset<TestRecord> = WindowChangeset::default();
+
+        assert_eq!(records.len(), 3);
+        assert_eq!(container.len(), 3);
+
+        container.remove_right(&mut changeset, 1, 1, vec![]);
+
+        assert_eq!(container.len(), 2);
+        assert_eq!(container.data[&records[0].get_id()], records[0]);
+        assert_eq!(container.data[&records[2].get_id()], records[2]);
+        assert_eq!(container.order[0], records[0].get_id());
+        assert_eq!(container.order[1], records[2].get_id());
+        assert!(changeset.widgets_to_remove.contains(&records[1].get_id()));
+    }
+
+    #[test]
+    fn remove_last_element_insert_0() {
+        let TestData{ records, mut container } = TestData::new(RECORDS_CNT, MAX_SIZE);
+        let mut changeset: WindowChangeset<TestRecord> = WindowChangeset::default();
+
+        assert_eq!(records.len(), 3);
+        assert_eq!(container.len(), 3);
+
+        container.remove_right(&mut changeset, 2, 1, vec![]);
+
+        assert_eq!(container.len(), 2);
+        assert_eq!(container.data[&records[0].get_id()], records[0]);
+        assert_eq!(container.data[&records[1].get_id()], records[1]);
+        assert_eq!(container.order[0], records[0].get_id());
+        assert_eq!(container.order[1], records[1].get_id());
+        assert!(changeset.widgets_to_remove.contains(&records[2].get_id()));
+    }
+
+    #[test]
+    fn remove_first_element_insert_1() {
+        let TestData{ records, mut container } = TestData::new(RECORDS_CNT, MAX_SIZE);
+        let mut changeset: WindowChangeset<TestRecord> = WindowChangeset::default();
+        let new_data = vec![
+            TestRecord::since("Added record 1", 1)
+        ];
+
+        assert_eq!(records.len(), 3);
+        assert_eq!(container.len(), 3);
+
+        println!("Container: {:#?}", container);
+
+        container.remove_right(&mut changeset, 0, 1, new_data.clone());
+
+        println!("Container: {:#?}", container);
+
+        assert_eq!(container.len(), 3);
+        assert_eq!(container.data[&records[1].get_id()], records[1]);
+        assert_eq!(container.data[&records[2].get_id()], records[2]);
+        assert_eq!(container.data[&new_data[0].get_id()], new_data[0]);
+        assert_eq!(container.order[0], records[1].get_id());
+        assert_eq!(container.order[1], records[2].get_id());
+        assert_eq!(container.order[2], new_data[0].get_id());
+        assert!(changeset.widgets_to_remove.contains(&records[0].get_id()));
+    }
+
+    #[test]
+    fn remove_second_element_insert_1() {
+        let TestData{ records, mut container } = TestData::new(RECORDS_CNT, MAX_SIZE);
+        let mut changeset: WindowChangeset<TestRecord> = WindowChangeset::default();
+        let new_data = vec![
+            TestRecord::since("Added record 1", 1)
+        ];
+
+        assert_eq!(records.len(), 3);
+        assert_eq!(container.len(), 3);
+
+        println!("Container: {:#?}", container);
+
+        container.remove_right(&mut changeset, 1, 1, new_data.clone());
+
+        println!("Container: {:#?}", container);
+
+        assert_eq!(container.len(), 3);
+        assert_eq!(container.data[&records[0].get_id()], records[0]);
+        assert_eq!(container.data[&records[2].get_id()], records[2]);
+        assert_eq!(container.data[&new_data[0].get_id()], new_data[0]);
+        assert_eq!(container.order[0], records[0].get_id());
+        assert_eq!(container.order[1], records[2].get_id());
+        assert_eq!(container.order[2], new_data[0].get_id());
+        assert!(changeset.widgets_to_remove.contains(&records[1].get_id()));
+    }
+
+    #[test]
+    fn remove_last_element_insert_1() {
+        let TestData{ records, mut container } = TestData::new(RECORDS_CNT, MAX_SIZE);
+        let mut changeset: WindowChangeset<TestRecord> = WindowChangeset::default();
+        let new_data = vec![
+            TestRecord::since("Added record 1", 1)
+        ];
+
+        assert_eq!(records.len(), 3);
+        assert_eq!(container.len(), 3);
+
+        println!("Container: {:#?}", container);
+
+        container.remove_right(&mut changeset, 2, 1, new_data.clone());
+
+        println!("Container: {:#?}", container);
+
+        assert_eq!(container.len(), 3);
+        assert_eq!(container.data[&records[0].get_id()], records[0]);
+        assert_eq!(container.data[&records[1].get_id()], records[1]);
+        assert_eq!(container.data[&new_data[0].get_id()], new_data[0]);
+        assert_eq!(container.order[0], records[0].get_id());
+        assert_eq!(container.order[1], records[1].get_id());
+        assert_eq!(container.order[2], new_data[0].get_id());
+        assert!(changeset.widgets_to_remove.contains(&records[2].get_id()));
+    }
+
+    #[test]
+    fn remove_first_element_insert_2() {
+        let TestData{ records, mut container } = TestData::new(RECORDS_CNT, MAX_SIZE);
+        let mut changeset: WindowChangeset<TestRecord> = WindowChangeset::default();
+        let new_data = vec![
+            TestRecord::since("Added record 1", 1),
+            TestRecord::since("Added record 2", 1),
+        ];
+
+        assert_eq!(records.len(), 3);
+        assert_eq!(container.len(), 3);
+
+        println!("Container: {:#?}", container);
+
+        container.remove_right(&mut changeset, 0, 1, new_data.clone());
+
+        println!("Container: {:#?}", container);
+
+        assert_eq!(container.len(), 4);
+        assert_eq!(container.data[&records[1].get_id()], records[1]);
+        assert_eq!(container.data[&records[2].get_id()], records[2]);
+        assert_eq!(container.data[&new_data[0].get_id()], new_data[0]);
+        assert_eq!(container.data[&new_data[1].get_id()], new_data[1]);
+        assert_eq!(container.order[0], records[1].get_id());
+        assert_eq!(container.order[1], records[2].get_id());
+        assert_eq!(container.order[2], new_data[0].get_id());
+        assert_eq!(container.order[3], new_data[1].get_id());
+        assert!(changeset.widgets_to_remove.contains(&records[0].get_id()));
+    }
+
+    #[test]
+    fn remove_second_element_insert_2() {
+        let TestData{ records, mut container } = TestData::new(RECORDS_CNT, MAX_SIZE);
+        let mut changeset: WindowChangeset<TestRecord> = WindowChangeset::default();
+        let new_data = vec![
+            TestRecord::since("Added record 1", 1),
+            TestRecord::since("Added record 2", 1),
+        ];
+
+        assert_eq!(records.len(), 3);
+        assert_eq!(container.len(), 3);
+
+        println!("Container: {:#?}", container);
+
+        container.remove_right(&mut changeset, 1, 1, new_data.clone());
+
+        println!("Container: {:#?}", container);
+
+        assert_eq!(container.len(), 4);
+        assert_eq!(container.data[&records[0].get_id()], records[0]);
+        assert_eq!(container.data[&records[2].get_id()], records[2]);
+        assert_eq!(container.data[&new_data[0].get_id()], new_data[0]);
+        assert_eq!(container.data[&new_data[1].get_id()], new_data[1]);
+        assert_eq!(container.order[0], records[0].get_id());
+        assert_eq!(container.order[1], records[2].get_id());
+        assert_eq!(container.order[2], new_data[0].get_id());
+        assert_eq!(container.order[3], new_data[1].get_id());
+        assert!(changeset.widgets_to_remove.contains(&records[1].get_id()));
+    }
+
+    #[test]
+    fn remove_last_element_insert_2() {
+        let TestData{ records, mut container } = TestData::new(RECORDS_CNT, MAX_SIZE);
+        let mut changeset: WindowChangeset<TestRecord> = WindowChangeset::default();
+        let new_data = vec![
+            TestRecord::since("Added record 1", 1),
+            TestRecord::since("Added record 2", 1),
+        ];
+
+        assert_eq!(records.len(), 3);
+        assert_eq!(container.len(), 3);
+
+        println!("Container: {:#?}", container);
+
+        container.remove_right(&mut changeset, 2, 1, new_data.clone());
+
+        println!("Container: {:#?}", container);
+
+        assert_eq!(container.len(), 4);
+        assert_eq!(container.data[&records[0].get_id()], records[0]);
+        assert_eq!(container.data[&records[1].get_id()], records[1]);
+        assert_eq!(container.data[&new_data[0].get_id()], new_data[0]);
+        assert_eq!(container.data[&new_data[1].get_id()], new_data[1]);
+        assert_eq!(container.order[0], records[0].get_id());
+        assert_eq!(container.order[1], records[1].get_id());
+        assert_eq!(container.order[2], new_data[0].get_id());
+        assert_eq!(container.order[3], new_data[1].get_id());
+        assert!(changeset.widgets_to_remove.contains(&records[2].get_id()));
+    }
+}
